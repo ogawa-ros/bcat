cd04870dbcf8afc23a2392002a600c9721ed2078
ayu-nico <ayu.kori818@gmail.com>
2021-01-14 20:18:24 +0900

    edit stage2_container.py & structure.py

--- bcat/stage2/stage2_container.py
+++ bcat/stage2/stage2_container.py
@@ -1,12 +1,10 @@
-
-import bcat.stage1.imaging
+#import bcat.stage1.imaging
 
 class stage2_container:
-    
+
     def __init__(self, data):
         self.data = data
         pass
 
     def imaging(self):
         return bcat.stage2.imaging.imaging(self.data)
-
--- bcat/structure.py
+++ bcat/structure.py
@@ -1,3 +1,4 @@
 
 from bcat.common.common_structure import freq_axis
 from bcat.stage1.stage1_structure import stage1_data_structure as stage1_data
+from bcat.stage2.stage2_structure import stage2_data_structure as stage2_data
