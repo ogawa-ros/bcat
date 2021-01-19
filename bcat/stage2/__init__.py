d341f77c9bf9c27bd86e80c5805bb3442d389b1c
Nishimura, Atsushi <ars096@gmail.com>
2021-01-19 16:37:50 +0900

    create nro45 io module

--- bcat/stage2/__init__.py
+++ bcat/stage2/__init__.py
@@ -1,2 +1,2 @@
 from bcat.stage2.stage2_structure import stage2_data_structure as data_structure
-from bcat.stage2.stage2_container import stage2_container as cnotainer
+from bcat.stage2.stage2_container import stage2_container as container
