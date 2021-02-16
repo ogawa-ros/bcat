352154b2ce1e7d21170ae5a8eccdc987e6722cd3
ayu-nico <ayu.kori818@gmail.com>
2021-02-17 01:42:02 +0900

    edit chopper_wheel

--- bcat/stage1/chopper_wheel.py
+++ bcat/stage1/chopper_wheel.py
@@ -21,21 +21,17 @@ class stage1_chopper_wheel(object):
         obstime_on = self.d.coord.obstime[on_mask]
 
         hot_set = numpy.split(hot_mask[0], numpy.where(numpy.diff(hot_mask[0]) != 1)[0]+1)
-        spectrum_hot = [numpy.average(self.d.spectrum[hot_set[i]],axis=0) for i in range(len(hot_set)) ]
+        spectrum_hot = numpy.array([numpy.average(self.d.spectrum[hot_set[i]],axis=0) for i in range(len(hot_set)) ])
         obstime_hot  = [self.d.coord.obstime[hot_set[i]][int(len(hot_set[i])/2)] for i in range(len(hot_set))]
 
         off_set = numpy.split(off_mask[0], numpy.where(numpy.diff(off_mask[0]) != 1)[0]+1)
-        spectrum_off = [numpy.average(self.d.spectrum[off_set[i]],axis=0) for i in range(len(off_set)) ]
+        spectrum_off = numpy.array([numpy.average(self.d.spectrum[off_set[i]],axis=0) for i in range(len(off_set)) ])
         obstime_off  = [self.d.coord.obstime[off_set[i]][int(len(off_set[i])/2)] for i in range(len(off_set))]
 
         coord_on = self.d.coord[on_mask]
 
         obstime_hot  = astropy.time.Time([self.d.coord.obstime[hot_set[i]][int(len(hot_set[i])/2)] for i in range(len(hot_set))])
         obstime_off  = astropy.time.Time([self.d.coord.obstime[off_set[i]][int(len(off_set[i])/2)] for i in range(len(off_set))])
-
-        spectrum_hot = numpy.array([numpy.average(self.d.spectrum[hot_set[i]],axis=0) for i in range(len(hot_set)) ])
-        spectrum_off = numpy.array([numpy.average(self.d.spectrum[off_set[i]],axis=0) for i in range(len(off_set)) ])
-
 
         Tas = []
         for i in range(len(spectrum_on)):
